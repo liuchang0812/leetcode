16bb96ec35536324b4720d6a4b626c361b4aa204
whu_liuchang <liuchang0812@gmail.com>
2014-06-11 15:12:38 +0800

    Update Reverse_Words_in_a_String.py

--- Reverse_Words_in_a_String.py
+++ Reverse_Words_in_a_String.py
@@ -1,8 +1,34 @@
+import unittest
+
 class Solution:
     # @param s, a string
     # @return a string
     def reverseWords(self, s):
-        # trip string
+        # strip string
         words = s.strip().split()[::-1]
         words = map(lambda x: x+" ", words)
         return ''.join(words).strip()
+
+def reverseWordsTest(unittest.TestCase):
+    def __init__(self):
+        self.solution = Solution()
+    
+    def test_blank(self):
+        input_str = ''
+        unittest.assertEqual(self.solution.reverseWords(input_str), "")
+    
+    def test_strip(self):
+        input_str = ' a b '
+        unittest.assertEqual(self.solution.reversWords(input_str), 'b a')
+        
+    def test_multiblank(self):
+        input_str = ' a  b'
+        unittest.assertEqual(self.solution.reversWords(input_str), 'b a')
+    
+    def test_tabblank(self):
+        input_str = '    a  b'
+        unittest.assertEqual(self.solution.reversWords(input_str), 'b a')
+
+if __name__ == "__main__":
+    unittest.main()
+        
