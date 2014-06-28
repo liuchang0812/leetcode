4219b4a8c64cd833bfe49ae9a03e36dc5fd0aec2
whu_liuchang <liuchang0812@gmail.com>
2014-06-28 23:45:52 +0800

    Update merge_k_sorted_lists.cpp
    加了堆，可以过

--- merge_k_sorted_lists.cpp
+++ merge_k_sorted_lists.cpp
@@ -1,3 +1,4 @@
+// TLE
 class Solution {
 public:
     ListNode *mergeKLists(vector<ListNode *> &lists) {
@@ -35,6 +36,46 @@ public:
             currentNode->next = NewNode;
             currentNode = NewNode;
             *minNode = (*minNode)->next;
+        }
+        return sortedList->next;
+    }
+};
+
+// heap O(NLOGK)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+#include <queue>
+#include <algorithm>
+
+class Solution {
+public:
+    ListNode *mergeKLists(vector<ListNode *> &lists) {
+        ListNode *sortedList = new ListNode(-1), *currentNode = NULL;
+        currentNode = sortedList;
+        size_t len = lists.size();
+        int minValue;
+        ListNode** minNode = NULL;
+        priority_queue<int, vector<int>, greater<int>> heap;
+        for (size_t i = 0;i < len;i ++)
+        {
+            while (lists[i] != NULL)
+            {
+                heap.push(lists[i]->val);
+                lists[i] = lists[i]->next;
+            }
+        }
+        while( !heap.empty())
+        {
+            ListNode* NewNode = new ListNode(heap.top());
+            heap.pop();
+            currentNode->next = NewNode;
+            currentNode = NewNode;
         }
         return sortedList->next;
     }
