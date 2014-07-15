3e1415e1fba14093b0c69bf1d4ccbaea621f05cf
whu_liuchang <liuchang0812@gmail.com>
2014-07-15 20:40:09 +0800

    Update subsets.cpp

--- subsets.cpp
+++ subsets.cpp
@@ -25,3 +25,37 @@ public:
         return anss;
     }
 };
+
+/*
+正确版
+*/
+
+class Solution {
+public:
+    vector<int> selected;
+    vector<vector<int> >ans;
+    void dfs(int index,vector<int>& nums, int used, int target)
+    {
+        if( used == target)
+        {
+            ans.push_back(selected);
+            return;
+        }
+        if(index == nums.size())
+            return;
+        // yao
+        selected.push_back(nums[index]);
+        dfs(index+1,nums,used+1,target);
+        selected.pop_back();
+        // bu yao
+        dfs(index+1,nums,used,target);
+    }
+    vector<vector<int> > subsets(vector<int> &S) {
+        sort(S.begin(), S.end());
+        for(int i = 0;i <= S.size();i ++)
+        {
+            dfs(0, S, 0, i);
+        }
+        return ans;
+    }
+};
