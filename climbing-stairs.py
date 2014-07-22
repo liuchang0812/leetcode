d19c52b2dd54c1dda132bdfc76f6d478bdad047e
whu_liuchang <liuchang0812@gmail.com>
2014-07-22 10:53:49 +0800

    Create climbing-stairs.py

---
+++ climbing-stairs.py
@@ -0,0 +1,10 @@
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def climbStairs(self, n):
+        a,b = 1,1
+        for i in range(1,n):
+            a,b = a+b,a
+            
+        return a
+        
