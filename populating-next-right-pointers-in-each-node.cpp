d42324ede1a387195181a5ed98c08d76388d2a85
liuchang0812 <liuchang0812@gmail.com>
2014-10-09 00:51:53 +0800

    fix compile error

--- populating-next-right-pointers-in-each-node.cpp
+++ populating-next-right-pointers-in-each-node.cpp
@@ -1,11 +1,13 @@
-/**
- * Definition for binary tree with next pointer.
- * struct TreeLinkNode {
- *  int val;
- *  TreeLinkNode *left, *right, *next;
- *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
- * };
- */
+#include <vector>
+#include <iostream>
+using namespace std;
+
+struct TreeLinkNode {
+  int val;
+  TreeLinkNode *left, *right, *next;
+  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+};
+
 class Solution {
 public:
     int numoflev[1005];
@@ -14,7 +16,7 @@ public:
     void connect(TreeLinkNode *root) {
         maxlev=-1;
         bfs(root, 0);
-        for (int lev =0; i<=maxlev;i++)
+        for (int lev =0; lev<=maxlev;lev++)
             putnext(lev);
     }
 private:
@@ -33,8 +35,9 @@ private:
         int len = ptrsoflev[lev].size();
         for (int i=0;i < len; i++)
         {
-            if (i==len-1) ptrsoflev[lev][i].next=NULL;
-            else ptrsoflev[lev][i].next = ptrsoflev[lev][i+1];
+            if (i==len-1)
+		    ptrsoflev[lev][i]->next=NULL;
+            else ptrsoflev[lev][i]->next = ptrsoflev[lev][i+1];
         }
     }
 };
