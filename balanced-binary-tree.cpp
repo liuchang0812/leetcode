e573af444f1fcac08cf76cdb98733d6b0cabba37
whu_liuchang <nfcj000@qq.com>
2014-10-18 20:44:47 +0800

    Update balanced-binary-tree.cpp

--- balanced-binary-tree.cpp
+++ balanced-binary-tree.cpp
@@ -1,13 +1,14 @@
-
 class Solution {
 public: 
     bool isBalanced(TreeNode* root) {
         if (root == NULL) return true;
-        return abs(deep(root->left) - deep(root->right)) <= 1; 
+        return (abs(deep(root->left) - deep(root->right)) <= 1) &&
+                isBalanced(root->left) &&
+                isBalanced(root->right);
     }
 private:
     int deep(TreeNode* root) {
         if (root == NULL) return 0;
-        return max(root->left, root->right) + 1;
+        return max(deep(root->left), deep(root->right)) + 1;
     }
 };
