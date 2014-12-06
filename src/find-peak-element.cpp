a469e823de502c380df7ec99430de14c98a715d1
whu_liuchang <nfcj000@qq.com>
2014-12-06 16:03:17 +0800

    Create find-peak-element.cpp

---
+++ src/find-peak-element.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int findPeakElement(const vector<int> &num) {
+        for (int i = 0;i < num.size(); i++)
+            if ( (i == 0 || num[i] > num[i-1])&&
+                (i== num.size()-1 || num[i] > num[i+1]))
+                return i;
+    }
+};
