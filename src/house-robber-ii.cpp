2dd0c7505c316c2704f3a064af67f9a74600c25e
whu_liuchang <nfcj000@qq.com>
2015-05-26 23:50:06 +0800

    Create house-robber-ii.cpp

---
+++ src/house-robber-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int rob(vector<int> &num) {
+        size_t len = num.size();
+        long long ans[len+5][2];
+        memset(ans, 0, sizeof ans);
+        if (num.size() == 0) 
+            return 0;
+        ans[0][0] = 0;
+        ans[0][1] = num[0];
+        for (size_t i=1; i<len; i++)
+        {
+            ans[i][0] = max(ans[i-1][1], ans[i-1][0]);
+            ans[i][1] = ans[i-1][0] + num[i];
+        }
+        long long fans = max(ans[0][1], ans[len-1][0]);
+        ans[0][0] = 0;
+        ans[0][1] = 0;
+        for (size_t i=1; i<len; i++)
+        {
+            ans[i][0] = max(ans[i-1][1], ans[i-1][0]);
+            ans[i][1] = ans[i-1][0] + num[i];
+        }
+        return max(fans, max(ans[len-1][0], ans[len-1][1]));
+    }
+};
