e1c3c3d21e739756e17b2ef9154f08d2c73f388d
whu_liuchang <nfcj000@qq.com>
2015-09-22 21:22:42 +0800

    Create move-zeroes.cpp

---
+++ src/move-zeroes.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i,j;
+        i = j = 0;
+        while (i < nums.size())
+        {
+            if (nums[i] != 0)
+                nums[j++] = nums[i];
+            i++;
+        }
+        while (j < nums.size())
+        {
+            nums[j] = 0;
+            j++;
+        }
+    }
+};
