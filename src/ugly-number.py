8edf3e0044543a436f4f78377a8e02ebed966e53
whu_liuchang <nfcj000@qq.com>
2015-09-23 11:23:08 +0800

    Create ugly-number.py

---
+++ src/ugly-number.py
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isUgly(int num) {
+        if (num == 0) return false;
+        if (num % 2==0)
+            return isUgly(num/2);
+        if (num % 3==0)
+            return isUgly(num/3);
+        if (num % 5==0)
+            return isUgly(num/5);
+        return num == 1;
+    }
+};
