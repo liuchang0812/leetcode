ee8d8068b029f2f17f1486114291d3d07580fef6
whu_liuchang <nfcj000@qq.com>
2016-04-19 14:16:35 +0800

    Create number-break.cpp

---
+++ src/number-break.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+        if (n == 2) return 1;
+        if (n == 3) return 2;
+        if (n == 4) return 4;
+        if (n == 5) return 6;
+        if (n == 6) return 9;
+        if (n == 7) return 12;
+        return integerBreak(n-3) * 3;
+    }
+};
