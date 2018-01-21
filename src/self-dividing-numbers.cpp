a1c27dfdfccc21759c111041aad1b61855705360
Chang Liu <liuchang0812@gmail.com>
2018-01-22 00:48:59 +0800

    new file self dividing numbers
    Signed-off-by: Chang Liu <liuchang0812@gmail.com>

--- src/self-dividing-numbers.cpp
+++ src/self-dividing-numbers.cpp
@@ -2,14 +2,19 @@ class Solution {
 public:
     bool isSelf(int x) {
         vector<int> digits;
+        int y = x;
         while (x > 0) 
         {
             digits.push_back(x%10);
+            cout << x % 10 << endl;
             x /= 10;
+            
         }
-        for (auto i : digits) if (i==0 || x % i == 0) return false;
+        for (auto i : digits) 
+            if (i==0 || y % i != 0) return false;
         return true;
     }
+    
     vector<int> selfDividingNumbers(int left, int right) {
         vector<int> ans;
         for (int i=left; i<=right;++i)
