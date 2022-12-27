ae998bc52260c1b1629b313af545426ce1d874f8
Chang Liu <liuchang0812@gmail.com>
2022-12-27 22:23:16 +0800

    Create 2027.cpp

---
+++ src/2027.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minimumMoves(string s) {
+        int ans = 0;
+        for (int i=0; i<s.size(); ) {
+            if (s[i] == 'X') {
+                ++ans;
+                i += 3;
+            } else {
+                ++i;
+            }
+        }
+        return ans;
+    }
+};
